465e5aebdeffc0f7a6536f9d7716409618d41aa7
Oncescu Laurentiu <larioncescu@yahoo.com>
2022-09-08 18:44:45 +0300

    make bleeding effect work

--- src/components/section-title/section-title.style.js
+++ src/components/section-title/section-title.style.js
@@ -4,18 +4,19 @@ export const Wrapper = styled.div`
   padding: 10px;
   background-color: #d52027;
   height: 60px;
-  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.16);
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.2);
   position: relative;
-  z-index: 2;
 
   &::before {
     content: "";
-    height: 60px;
-    width: 35px;
-    background-color: inherit;
+    background-color: #d52027;
     position: absolute;
-    right: -35px;
     top: 0;
+    bottom: 0;
+    right: -35px;
+    left: -35px;
+    box-shadow: 0 2px 6px rgba(0, 0, 0, 0.2);
+    z-index: -1;
   }
 `;
 
