204feb5dbb73134c3c700fd51f5e2b8e4ed961c7
Oncescu Laurentiu <larioncescu@yahoo.com>
2022-09-09 18:22:48 +0300

    add section icon

--- src/components/welcome/welcome.component.jsx
+++ src/components/welcome/welcome.component.jsx
@@ -1,10 +1,17 @@
 import GridRow from "../grid/grid-row";
 import SectionTitle from "../section-title";
+import SectionIcon from "../section-icon";
 
 function Welcome() {
   return (
     <>
       <SectionTitle text="Welcome to my CV" />
+      <GridRow gridColumn="1/-1">
+        <SectionIcon type="red" />
+      </GridRow>
+      <GridRow gridColumn="1/-1">
+        <SectionIcon type="green" />
+      </GridRow>
     </>
   );
 }
