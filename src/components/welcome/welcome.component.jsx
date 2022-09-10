1d9c2104778d4dd9b4fcfe455379b15b0697bb97
Oncescu Laurentiu <larioncescu@yahoo.com>
2022-09-10 16:59:40 +0300

    add name

--- src/components/welcome/welcome.component.jsx
+++ src/components/welcome/welcome.component.jsx
@@ -2,12 +2,17 @@ import GridRow from "../grid/grid-row";
 import SectionTitle from "../section-title";
 import SectionIcon from "../section-icon";
 
+import * as S from "./welcome.style";
+
 function Welcome() {
   return (
     <>
       <SectionTitle text="Welcome to my CV" />
       <GridRow gridColumn="1/-1">
         <SectionIcon type="red" />
+      </GridRow>
+      <GridRow gridColumn={"1/-1"}>
+        <S.Name>Laurentiu Oncescu</S.Name>
       </GridRow>
       <GridRow gridColumn="1/-1">
         <SectionIcon type="green" />
--- src/components/welcome/welcome.style.js
+++ src/components/welcome/welcome.style.js
@@ -1,1 +1,9 @@
 import styled from "styled-components";
+
+export const Name = styled.h1`
+  font-size: 3rem;
+  line-height: 64px;
+  color: #707070;
+  font-weight: 500;
+  text-align: center;
+`;
