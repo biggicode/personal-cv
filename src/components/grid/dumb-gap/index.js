46ce02bd64ab7868c4e527b46dc6652fb842f44e
Oncescu Laurentiu <larioncescu@yahoo.com>
2022-09-10 18:03:34 +0300

    add dumb gap

---
+++ src/components/grid/dumb-gap/index.js
@@ -0,0 +1,1 @@
+export { default } from "./dumb-gap.component";
--- src/components/welcome/welcome.component.jsx
+++ src/components/welcome/welcome.component.jsx
@@ -5,6 +5,7 @@ import SemiTitle from "../semi-title";
 import PillButton from "../pill-button";
 
 import * as S from "./welcome.style";
+import DumbGap from "../grid/dumb-gap";
 
 function Welcome() {
   return (
@@ -19,7 +20,11 @@ function Welcome() {
       <GridRow gridColumn={"1/-1"}>
         <S.JobTitle>Junior Web Developer</S.JobTitle>
       </GridRow>
+
+      <DumbGap />
+
       <SemiTitle text="About me" />
+
       <GridRow gridColumn={"1/-1"}>
         <S.DescriptionBox>
           <S.DescriptionText>
@@ -27,6 +32,9 @@ function Welcome() {
           </S.DescriptionText>
         </S.DescriptionBox>
       </GridRow>
+
+      <DumbGap />
+
       <GridRow gridColumn={"1/-1"}>
         <PillButton icon="download" url={"blabla"} text="Download CV" />
       </GridRow>
