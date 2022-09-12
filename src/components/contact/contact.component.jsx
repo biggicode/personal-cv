89066d809ae902788c3413414d61e2cf6cb3bd8c
Oncescu Laurentiu <larioncescu@yahoo.com>
2022-09-12 22:19:08 +0300

    add profile img

--- src/components/contact/contact.component.jsx
+++ src/components/contact/contact.component.jsx
@@ -1,6 +1,10 @@
 import SectionTitle from "../section-title";
 import SectionIcon from "../section-icon";
 import GridRow from "../grid/grid-row";
+import DumbGap from "../grid/dumb-gap";
+
+import * as S from "./contact.style";
+import profileImg from "../../assets/img/cv2.jpg";
 
 function Contact({ data }) {
   return (
@@ -9,6 +13,13 @@ function Contact({ data }) {
       <GridRow gridColumn="1/-1">
         <SectionIcon type="red" />
       </GridRow>
+      <GridRow gridColumn="span 4">
+        <S.ImgWrapper>
+          <S.StyledImg src={profileImg} />
+        </S.ImgWrapper>
+      </GridRow>
+
+      <DumbGap />
     </>
   );
 }
---
+++ src/components/contact/contact.style.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const ImgWrapper = styled.div`
+  padding: 10px;
+  background-color: #fff;
+  border-radius: 8px;
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.25);
+`;
+
+export const StyledImg = styled.img`
+  border-radius: 6px;
+  width: 100%;
+`;
