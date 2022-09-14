caf5acb0b90e3828d0381333549626b16b72e022
Oncescu Laurentiu <larioncescu@yahoo.com>
2022-09-14 23:01:55 +0300

    handle selected

--- src/components/contact/contact.component.jsx
+++ src/components/contact/contact.component.jsx
@@ -1,12 +1,20 @@
+import { useState } from "react";
 import SectionTitle from "../section-title";
 import SectionIcon from "../section-icon";
 import GridRow from "../grid/grid-row";
 import DumbGap from "../grid/dumb-gap";
+import PillButton from "../pill-button";
 
 import * as S from "./contact.style";
 import profileImg from "../../assets/img/cv2.jpg";
 
 function Contact({ data }) {
+  const [selected, setSelected] = useState();
+
+  const handleContact = (icon) => {
+    setSelected(data.buttonList.find((button) => button.icon === icon));
+  };
+
   return (
     <>
       <SectionTitle text={data.sectionTitle} />
@@ -21,23 +29,36 @@ function Contact({ data }) {
 
       <DumbGap />
       {data.buttonList.map((button) => (
-        <S.ContactButton>
+        <S.ContactButton
+          key={button.icon}
+          onClick={() => {
+            handleContact(button.icon);
+          }}
+          id={button.icon}
+        >
           <S.StyledIcon className={`icon-${button.icon}`} />
         </S.ContactButton>
       ))}
 
       <DumbGap />
-
-      <GridRow gridColumn="1/-1">
-        <S.ContactBox>
-          <S.BoxHeader>
-            <p>Merge</p>
-            <S.CrossIcon className="icon-cross" />
-          </S.BoxHeader>
-          <S.BoxContent>sdfidsfjni</S.BoxContent>
-          <S.BoxFooter>This is footer</S.BoxFooter>
-        </S.ContactBox>
-      </GridRow>
+      {selected && (
+        <GridRow gridColumn="1/-1">
+          <S.ContactBox>
+            <S.BoxHeader>
+              <p>{selected.title}</p>
+              <S.CrossIcon className="icon-cross" />
+            </S.BoxHeader>
+            <S.BoxContent>{selected.description}</S.BoxContent>
+            <S.BoxFooter>
+              <PillButton
+                icon="link"
+                text={selected.label}
+                url={selected.link}
+              />
+            </S.BoxFooter>
+          </S.ContactBox>
+        </GridRow>
+      )}
 
       <GridRow gridColumn="1/-1">
         <SectionIcon type="green" />
